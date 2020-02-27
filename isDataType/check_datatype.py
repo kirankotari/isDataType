2b5b61175cd367f641ce51f58834eecb9df52274
Kiran Kumar Kotari <kirankotari@live.com>
2020-02-27 15:39:02 +0530

    adding isListInt, isListFloat, isListString, toBool, toInt, toFloat

--- isDataType/check_datatype.py
+++ isDataType/check_datatype.py
@@ -1,13 +1,13 @@
 from distutils.util import strtobool
 import logging
 
-def isInt(arg):
+def isInt(arg, remove_decimals=False):
     try:
         float(arg)
     except ValueError:
         return False
     else:
-        return float(arg).is_integer()
+        return float(arg).is_integer() if remove_decimals == False else True
 
 def isFloat(arg, allow_zeros=True):
     try:
@@ -29,33 +29,14 @@ def isBool(arg):
         strtobool(arg)
     except ValueError:
         return False
-    # TODO: need to use this on convertion bool(strtobool(arg))
     return True
 
-def isList(arg):
-    pass
+def isListInt(argList, remove_decimals=False):
+    return all(isInt(each_arg, remove_decimals) for each_arg in argList)
 
-def isDict(arg):
-    pass
+def isListFloat(argList, allow_zeros=True):
+    return all(isFloat(each_arg, allow_zeros) for each_arg in argList)
 
-def isSet(arg):
-    pass
-
-def isTuple(arg):
-    pass
-
-def isComplex(arg):
-    pass
-
-def isListInt(arg):
-    pass
-
-def isListFloat(arg):
-    pass
-
-def isListString(arg):
-    pass
-
-def isListDict(arg):
-    pass
+def isListString(argList):
+    return all(isString(each_arg) for each_arg in argList)
 
--- isDataType/typecast.py
+++ isDataType/typecast.py
@@ -1,11 +1,24 @@
 from distutils.util import strtobool
 import logging
+from .check_datatype import isListInt, isListFloat, isListString, isBool
 
 def toBool(arg):
-    try:
-        strtobool(arg)
-    except ValueError as e:
-        # TODO: need to log the messages.
-        return None
-    else:
+    if isBool(arg):
         return bool(strtobool(arg))
+    else:
+        logging.error("Invalid input, Error: {}".format(e))
+
+def toInt(argList, remove_decimals=False):
+    if isListInt(argList, remove_decimals):
+        return map(int, argList)
+    else:
+        logging.error("Invlaid input, Error: Couldn't able to convert")
+
+def toFloat(argList, allow_zeros=True):
+    if isListFloat(argList, allow_zeros):
+        return map(float, argList)
+    else:
+        logging.error("Invlaid input, Error: Couldn't able to convert")
+
+def toDynamicTypeCast(argList):
+    pass
